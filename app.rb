b138f8d7c3c4a8d6f370c261edd0e30e3884b0e8
Senya Webster <senyawebster@Senyas-MacBook-Air-2.local>
2017-09-29 14:10:28 -0700

    add views for shoe and store, with framework established, waiting to be built out

---
+++ app.rb
@@ -0,0 +1,60 @@
+require("bundler/setup")
+require 'pry'
+Bundler.require(:default)
+
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get '/' do
+  @shoes = Shoe.all()
+  @stores = Store.all()
+  erb(:index)
+end
+
+get '/create_store' do
+  @shoes = Shoe.all()
+  @stores = Store.all()
+  erb(:index)
+end
+
+post '/create_store' do
+  name = params['name']
+  store = Store.create({:name => name})
+  @shoes = Shoe.all()
+  @stores = Store.all()
+  erb(:index)
+end
+
+
+
+
+# CURRENT ITEM TO WORK ON
+get '/store/:id' do
+  @store = Store.find(params[:id])
+  @shoes = Shoe.all()
+  @unstocked_shoes = @shoes - @store.shoes
+  erb(:store_info)
+end
+# END CURRENT ITEM TO WORK ON
+
+
+
+
+get '/create_shoe' do
+  @shoes = Shoe.all()
+  @stores = Store.all()
+  erb(:index)
+end
+
+post '/create_shoe' do
+  brand = params['brand']
+  price = params['price']
+  shoe = Shoe.create({:brand => brand, :price => price})
+  @shoes = Shoe.all().sort_by {|x| x.price}
+  @stores = Store.all()
+  erb(:index)
+end
+
+get '/shoe/:id' do
+
+  erb(:shoe_info)
+end
