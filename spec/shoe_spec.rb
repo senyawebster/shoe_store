b080ac88ee2aad7355c8ae03b6ac97011e2952f4
Senya Webster <senya.webster@gmail.com>
2017-10-04 03:56:51 -0700

    add passing spec empty shoe input

--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -2,4 +2,19 @@ require 'spec_helper'
 
 describe(Shoe) do
   it { should have_and_belong_to_many(:stores)}
+
+  it("validates presence of store brand") do
+    shoe = Shoe.new({:brand => ""})
+    expect(shoe.save()).to(eq(false))
+  end
+
+  # it("validates presence of description") do
+  #   shoe = Shoe.new({:description => ""})
+  #   expect(shoe.save()).to(eq(false))
+  # end
+  #
+  # it("validates presence of description") do
+  #   shoe = Shoe.new({:description => ""})
+  #   expect(shoe.save()).to(eq(false))
+  # end
 end
